01f3fdb29d7355723089a04adb31818a3b76c46f
Maximilian Birkenhagen <darkwolve.darkwolve@gmail.com>
2019-03-17 17:31:25 +0100

    spaces ==> tabs

--- classifier/test_dataset.py
+++ classifier/test_dataset.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
 if __name__ == "__main__":
-    image_index = 54050 # You may select anything up to 60,000
-    print(y_train[image_index]) # The label is 8
-    plt.imshow(x_train[image_index], cmap='Greys')
-    plt.show()
+	image_index = 54050 # You may select anything up to 60,000
+	print(y_train[image_index]) # The label is 8
+	plt.imshow(x_train[image_index], cmap='Greys')
+	plt.show()
