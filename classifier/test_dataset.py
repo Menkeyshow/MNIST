e8868689ff19ffc3fcbc4715695ca3e6144b2435
Maximilian Birkenhagen <darkwolve.darkwolve@gmail.com>
2019-03-18 23:34:58 +0100

    MNIST dataset to 4dim

--- classifier/test_dataset.py
+++ classifier/test_dataset.py
@@ -12,13 +12,16 @@ import matplotlib.pyplot as plt
 import classifier.util.ElapsedTime as ElapsedTime
 import classifier.models.classifier_model as classifier_model
 
-#Load Dataset
+#Load Dataset 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+#3dim ==> 4dim
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
 
 if __name__ == "__main__":
     Timer = ElapsedTime.ElapsedTime()
     print(x_train[0].shape)
-    model = classifier_model.model_object('Adam')
+    model = classifier_model.model_object('Adam', inputshape=x_train.shape[0])
     model.model.fit(x=x_train, y=y_train, epochs=10)
     image_index = 54050 # You may select anything up to 60,000
     print(y_train[image_index]) # The label is 8
