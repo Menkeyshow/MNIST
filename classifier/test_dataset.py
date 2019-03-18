ab4a38c08d00637b6b93f4e5713589436f65c1a3
Maximilian Birkenhagen <darkwolve.darkwolve@gmail.com>
2019-03-18 01:47:25 +0100

    import model

--- classifier/test_dataset.py
+++ classifier/test_dataset.py
@@ -1,19 +1,24 @@
 #%%
-""" https://towardsdatascience.com/image-classification-in-10-minutes-with-mnist-dataset-54c35b77a38d 
-	"""
+""" 
+https://towardsdatascience.com/image-classification-in-10-minutes-with-mnist-dataset-54c35b77a38d 
+"""
 
 import os
-print(os.getcwd())  # Prints the current working directory
+
+print('Make sure to use right WD:',os.getcwd())  # Prints the current working directory
 
 import keras
 import matplotlib.pyplot as plt
 import classifier.util.ElapsedTime as ElapsedTime
+import classifier.models.classifier_model as classifier_model
 
 #Load Dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
 if __name__ == "__main__":
     Timer = ElapsedTime.ElapsedTime()
+    print(x_train[0].shape)
+    model = classifier_model.model_object('Adam')
     image_index = 54050 # You may select anything up to 60,000
     print(y_train[image_index]) # The label is 8
     plt.imshow(x_train[image_index], cmap='Greys')
