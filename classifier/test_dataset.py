a208a1fee6cfc38cd533816bb62e6d6fa4d2449d
Maximilian Birkenhagen <darkwolve.darkwolve@gmail.com>
2019-03-19 00:09:53 +0100

    finished classifier, there may be bugs?

--- classifier/test_dataset.py
+++ classifier/test_dataset.py
@@ -18,13 +18,20 @@ import classifier.models.classifier_model as classifier_model
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
 x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
 
+
 if __name__ == "__main__":
     Timer = ElapsedTime.ElapsedTime()
-    print(x_train[0].shape)
     model = classifier_model.model_object('Adam', inputshape=(28,28,1))
     model.model.fit(x=x_train, y=y_train, epochs=10)
-    image_index = 54050 # You may select anything up to 60,000
-    print(y_train[image_index]) # The label is 8
-    plt.imshow(x_train[image_index], cmap='Greys')
-    plt.show()
+    model.model.evaluate(x=x_test, y=y_test)
+    print('Trainingsduration:')
     Timer.printTime()
+
+    #Test Result :D
+    print(y_train[54050])
+    plt.imshow(x_train[54050], cmap='Greys')
+    zahl = model.model.predict(y_train[54050].reshape(1, 28, 28, 1))
+    plt.show()
+    print('Netz sagt: ', zahl)
+
+    
