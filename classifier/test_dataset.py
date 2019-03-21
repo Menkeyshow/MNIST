b6074709fe1c70d347a3db6772165c9ed9cedbe2
Maximilian Birkenhagen <darkwolve.darkwolve@gmail.com>
2019-03-22 00:47:29 +0100

    refactored the whole classifier to be OOP

--- classifier/test_dataset.py
+++ classifier/test_dataset.py
@@ -5,35 +5,103 @@ https://towardsdatascience.com/image-classification-in-10-minutes-with-mnist-dat
 
 import os
 
-print('Make sure to use right WD:',os.getcwd())  # Prints the current working directory
+print('Make sure to use right WorkingDirectory, currently it is:\n', os.getcwd(), 'it should be: \n ...\\MNIST')  # Prints the current working directory
 
 import keras
 import matplotlib.pyplot as plt
 import numpy as np
-import classifier.util.ElapsedTime as ElapsedTime
+from classifier.util.ElapsedTime import ElapsedTime
 import classifier.models.classifier_model as classifier_model
 
-#Load Dataset 
-(x_train_pre, y_train), (x_test_pre, y_test) = keras.datasets.mnist.load_data()
-#3dim ==> 4dim
-x_train = x_train_pre.reshape(x_train_pre.shape[0], x_train_pre.shape[1], x_train_pre.shape[2], 1)
-x_test = x_test_pre.reshape(x_test_pre.shape[0], x_test_pre.shape[1], x_test_pre.shape[2], 1)
+class classifier(object):
+    def __init__(self, model, do_train=True, do_test=True, do_use_weights=False, do_save_weights=True, epochs=10):
+        self.timer = ElapsedTime()
+        
+        self.do_train = do_train
+        self.do_test = do_test
+        self.do_use_weights = do_use_weights
+        self.do_save_weights = do_save_weights
+        self.epochs = epochs
+
+        self.model = model
+        self.isDataPrepared = False
+        self.x_train = None
+        self.y_train = None
+        self.x_test = None
+        self.y_test = None
+
+        if(self.do_use_weights):
+            self.model.model.load_weights('weights.h5')
+            print('Loaded saved weights')
+    
+    def prepare_data(self, x_train, y_train, x_test, y_test, isNormalized=False):
+        #3dim ==> 4dim
+        x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+        x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
+        print('Data now reshaped into:', x_train.shape,'and', x_test.shape)
+
+        #normalize values between 0 and 1
+        if(not isNormalized):
+            x_train = x_train.astype('float32')
+            x_train /= 255
+
+            x_test = x_test.astype('float32')
+            x_test /= 255
+            print('Data now normalized!')
+        
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.isDataPrepared = True
+        
+    def train_model(self):
+        trainTimer = ElapsedTime()
+        print('Begin training!')
+
+        if(not self.isDataPrepared):
+            print('Data wasnt prepared before training/evaluating, aborting...')
+            return
+        
+        self.model.model.fit(x=self.x_train, y=self.y_train, epochs=self.epochs)
+        self.model.model.save_weights('weights.h5')
+        print('Done training!')
+        trainTimer.printTime()
+
+    def evaluate_model(self):
+        evalTimer = ElapsedTime()
+        print('Begin evaluating!')
+
+        scores = self.model.model.evaluate(x=self.x_test, y=y_test)
+
+        print('Done evaluating!')
+        evalTimer.printTime()
+        return scores
 
 
 if __name__ == "__main__":
-    Timer = ElapsedTime.ElapsedTime()
-    model = classifier_model.model_object('Adam', inputshape=(28,28,1))
-    #model.model.fit(x=x_train, y=y_train, epochs=10)
-    model.model.evaluate(x=x_test, y=y_test)
-    print('Trainingsduration:')
-    Timer.printTime()
+    # Load dataset MNIST
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
-    #Test Result :D
+    # Set up objects
+    model = classifier_model.model_object('Adam', inputshape=(28,28,1))
+    classifier = classifier(model, epochs=5)
+
+    # Train classifier
+    classifier.prepare_data(x_train, y_train, x_test, y_test, isNormalized=False)
+    classifier.train_model()
+
+    # Evaluate classifier
+    scores = classifier.evaluate_model()
+    print('Script-Duration: ', classifier.timer.getTime())
+    print(scores, '\n', classifier.model.model.metrics_names)
+
+    # Test Result :D
     print('Die Zahl ist: ', y_train[54050])
-    plt.imshow(x_train_pre[54050], cmap='Greys')
-    zahl = model.model.predict(x_train[54050].reshape(1, 28, 28, 1))
+    plt.imshow(x_train[54050], cmap='Greys')
+    prediction = classifier.model.model.predict(x_train[54050].reshape(1, 28, 28, 1))
     plt.show()
-    print(zahl)
-    print('Netz sagt: ', np.argmax(zahl))
+    print('Inhalt des Arrays: ',prediction)
+    print('Netz sagt: ', np.argmax(prediction))
 
     
